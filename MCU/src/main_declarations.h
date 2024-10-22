bda9009fbe070a7c8e80fc986d0281ec58d7d641
Fraser Holman <fraserholman1@gmail.com>
2024-10-22 14:09:42 +0100

    MCU and App Code

---
+++ MCU/src/main_declarations.h
@@ -0,0 +1,9 @@
+#ifndef main_declarations_h
+#define main_declarations_h
+
+// packages included
+#include <Arduino.h>
+
+// code here
+
+#endif
---
+++ MobileApplication/robotic_printer_gui/android/app/src/main/kotlin/com/example/robotic_printer_gui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.robotic_printer_gui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
