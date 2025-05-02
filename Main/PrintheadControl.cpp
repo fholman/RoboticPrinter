0a16b3640de0ff4fa99a80cf349478590085cda6
Fraser Holman <fraserholman1@gmail.com>
2025-05-02 17:48:01 +0100

    Minor Changes

--- Main/PrintheadControl.cpp
+++ Main/PrintheadControl.cpp
@@ -1,6 +1,9 @@
 #include "PrintheadControl.h"
 
 void PrintheadControl::printheadSetUp(){
+  gpio_reset_pin(GPIO_NUM_39);
+  gpio_reset_pin(GPIO_NUM_40);
+
   for(int i = 0; i < 12; i++){
     pinMode(nozzlePins[i], OUTPUT);
     digitalWrite(nozzlePins[i], LOW);
